ad117dd0aa8d5a9a970625143f04c4d6b78f20d5
Al <albarrentine@gmail.com>
2025-04-24 02:10:42 -0400

    [fix] uint32_array include

--- src/uint32_array.h
+++ src/uint32_array.h
@@ -7,7 +7,7 @@
 
 #define ARRAY_NAME uint32_array_aligned
 #define ARRAY_TYPE uint32_t
-#include "array_array/aligned_array.h"
+#include "aligned_array/aligned_array.h"
 #undef ARRAY_NAME
 #undef ARRAY_TYPE
 
